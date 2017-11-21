554e957e6245ba2271d44c709ba0733141e897ff
PFCKrutonium <PFCKrutonium@gmail.com>
2017-11-21 02:22:51 -0500

    OwO Whats This?

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OwO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                 *Notices Bulge*");
+            Console.WriteLine("__        ___           _    _        _   _     _      ");
+            Console.WriteLine("\\ \\      / / |__   __ _| |_ ( ) ___  | |_| |__ (_) ___ ");
+            Console.WriteLine(" \\ \\ /\\ / /| '_ \\ / _\\`| __|// / __| | __| '_ \\| |/ __|");
+            Console.WriteLine("  \\ V  V / | | | | (_| | |_    \\__ \\ | |_| | | | |\\__ \\");
+            Console.WriteLine("   \\_/\\_/  |_| |_|\\__,_|\\__|   |___/ \\___|_| |_|_|/___/");
+        }
+    }
+}
