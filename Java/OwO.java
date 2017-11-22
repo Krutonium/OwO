2c27a8330485d48b872a7f8b3993857cef5b7bfc
LRFLEW <lrflew@aol.com>
2017-11-21 21:48:14 -0600

    Add Bash and Java Variants

---
+++ Java/OwO.java
@@ -0,0 +1,12 @@
+public class OwO
+{
+    public static void main(String[] args)
+    {
+        System.out.println("                 *Notices Bulge*");
+        System.out.println("__        ___           _    _        _   _     _      ");
+        System.out.println("\\ \\      / / |__   __ _| |_ ( ) ___  | |_| |__ (_) ___ ");
+        System.out.println(" \\ \\ /\\ / /| '_ \\ / _\\`| __|// / __| | __| '_ \\| |/ __|");
+        System.out.println("  \\ V  V / | | | | (_| | |_    \\__ \\ | |_| | | | |\\__ \\");
+        System.out.println("   \\_/\\_/  |_| |_|\\__,_|\\__|   |___/ \\___|_| |_|_|/___/");
+    }
+}
