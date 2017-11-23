ca5041918396778305eb5092a878675c7dc36bc5
PFCKrutonium <PFCKrutonium@gmail.com>
2017-11-22 20:17:12 -0500

    Modified C# Source to make the message readable in source.

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -7,11 +7,11 @@ namespace OwO
         static void Main(string[] args)
         {
             Console.WriteLine("                 *Notices Bulge*");
-            Console.WriteLine("__        ___           _    _        _   _     _      ");
-            Console.WriteLine("\\ \\      / / |__   __ _| |_ ( ) ___  | |_| |__ (_) ___ ");
-            Console.WriteLine(" \\ \\ /\\ / /| '_ \\ / _\\`| __|// / __| | __| '_ \\| |/ __|");
-            Console.WriteLine("  \\ V  V / | | | | (_| | |_    \\__ \\ | |_| | | | |\\__ \\");
-            Console.WriteLine("   \\_/\\_/  |_| |_|\\__,_|\\__|   |___/ \\___|_| |_|_|/___/");
+            Console.WriteLine(@"__        ___           _    _        _   _     _      ");
+            Console.WriteLine(@"\ \      / / |__   __ _| |_ ( ) ___  | |_| |__ (_) ___ ");
+            Console.WriteLine(@" \ \ /\ / /| '_ \ / _\`| __|// / __| | __| '_ \| |/ __|");
+            Console.WriteLine(@"  \ V  V / | | | | (_| | |_    \__ \ | |_| | | | |\__ \");
+            Console.WriteLine(@"   \_/\_/  |_| |_|\__,_|\__|   |___/ \___|_| |_|_|/___/");
         }
     }
 }
