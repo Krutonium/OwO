6f7b97fd288c6212dabe247aed1d0d6af3b689ae
Hitesh Bhagchandani <hiteshbhagchandani39@gmail.com>
2017-12-21 23:12:22 +0530

    Add Haskell version

---
+++ Haskell/OwO.hs
@@ -0,0 +1,11 @@
+{-# OPTIONS_GHC -Wall -Werror #-}
+module Main where
+
+main :: IO ()
+main = do
+    putStrLn "                 *Notices Bulge*"
+    putStrLn "__        ___           _    _        _   _     _      "
+    putStrLn "\\ \\      / / |__   __ _| |_ ( ) ___  | |_| |__ (_) ___ "
+    putStrLn " \\ \\ /\\ / /| '_ \\ / _\\`| __|// / __| | __| '_ \\| |/ __|"
+    putStrLn "  \\ V  V / | | | | (_| | |_    \\__ \\ | |_| | | | |\\__ \\"
+    putStrLn "   \\_/\\_/  |_| |_|\\__,_|\\__|   |___/ \\___|_| |_|_|/___/"
