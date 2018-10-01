bb71eda653cfff068ff7c607b6c87a61f9e4e828
Jay Shah <Jay9596@users.noreply.github.com>
2018-10-02 01:13:40 +0530

    Add Go implementation

---
+++ Go/OwO.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("                 *Notices Bulge*")
+	fmt.Println("__        ___           _    _        _   _     _      ")
+	fmt.Println("\\ \\      / / |__   __ _| |_ ( ) ___  | |_| |__ (_) ___ ")
+	fmt.Println(" \\ \\ /\\ / /| '_ \\ / _\\`| __|// / __| | __| '_ \\| |/ __|")
+	fmt.Println("  \\ V  V / | | | | (_| | |_    \\__ \\ | |_| | | | |\\__ \\")
+	fmt.Println("   \\_/\\_/  |_| |_|\\__,_|\\__|   |___/ \\___|_| |_|_|/___/")
+}
