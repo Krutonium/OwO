fae01663974e3365f6602f91d9d1504873cd7a25
Kuldip Patel <patel.kuldip91@gmail.com>
2019-10-03 23:53:03 +0530

    added Ada Support

---
+++ Ada/OwO.adb
@@ -0,0 +1,10 @@
+with Ada.Text_IO; use Ada.Text_IO;
+procedure owo is
+begin
+  Put_Line ("                 *Notices Bulge*");
+  Put_Line ("__        ___           _    _        _   _     _      ");
+  Put_Line ("\\ \\      / / |__   __ _| |_ ( ) ___  | |_| |__ (_) ___ ");
+  Put_Line (" \\ \\ /\\ / /| '_ \\ / _\\`| __|// / __| | __| '_ \\| |/ __|");
+  Put_Line ("  \\ V  V / | | | | (_| | |_    \\__ \\ | |_| | | | |\\__ \\");
+  Put_Line ("   \\_/\\_/  |_| |_|\\__,_|\\__|   |___/ \\___|_| |_|_|/___/");
+end owo;
